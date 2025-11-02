09a0af1ac8730c71867b861e7b37779ce5ee9fb2
lukegjpotter <lukegjpotter@gmail.com>
2025-11-02 21:34:49 +0000

    Initial Commit

---
+++ src/main/java/com/lukegjpotter/cct/CCT_CA1_2_WebApplication/CctCa12WebApplication.java
@@ -0,0 +1,13 @@
+package com.lukegjpotter.cct.CCT_CA1_2_WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CctCa12WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CctCa12WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukegjpotter/cct/CCT_CA1_2_WebApplication/CctCa12WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukegjpotter.cct.CCT_CA1_2_WebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CctCa12WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
